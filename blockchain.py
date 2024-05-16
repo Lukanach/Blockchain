96847b112c9bc53a18cb09f79f04d75847972dc6
Lukanach <lukanach922@gmail.com>
2024-05-16 16:02:06 +0300

    Loop

--- blockchain.py
+++ blockchain.py
@@ -33,24 +33,27 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting block')
         print(block)
+    else:
+        print('-' * 20)
+
 
 def verify_chain():
     block_index = 0
     is_valid = True
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1
             continue
-        elif block[0] == blockchain[block_index - 1]:
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
             is_valid = True
         else:
             is_valid = False
             break
-        block_index += 1
     return is_valid
 
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
@@ -63,14 +66,17 @@ while True:
     elif user_choise == '2':
         print_blockchain_elements()
     elif user_choise == 'q':
-        break
+        waiting_for_input = False
     elif user_choise == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     else:
         print('Input is invalid')
     if not verify_chain():
+        print_blockchain_elements()
         print('Invalid blockchain')
         break
+else:
+    print('User left')
 
 print('Done!')
